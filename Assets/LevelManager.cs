94fd4f4f05580093698da416f26463e03af5d4f7
ianco99 <71154684+ianco99@users.noreply.github.com>
2023-05-13 19:06:10 -0300

    Added on end level panel

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class LevelManager : MonoBehaviour
 {
    [SerializeField] private PlayerTriggerDetector endGoalTrigger;
+    [SerializeField] private UnityEvent OnEndLevel;
 
     private void Awake()
     {
-        endGoalTrigger.OnPlayerTrigger += EndLevel;
+        endGoalTrigger.OnPlayerTrigger += () => OnEndLevel.Invoke();
     }
 
-    private void EndLevel()
-    {
-        GameManager.Instance.LoadGameLevel(true);
-    }
+    //private void EndLevel()
+    //{
+    //    GameManager.Instance.LoadGameLevel(true);
+    //}
 }
