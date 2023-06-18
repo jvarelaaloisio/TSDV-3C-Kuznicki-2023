c5aa83cc67b21d848e3db03c8de54bf42d8fe92b
ianco99 <71154684+ianco99@users.noreply.github.com>
2023-06-18 11:57:09 -0300

    Created jewel script

---
+++ Assets/Jewel.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jewel : MonoBehaviour, IAttackable, ITargetable
+{
+    [SerializeField] private ParticleSystem deathParticles;
+    [SerializeField] private Outline outline;
+
+    public void SetTargetted(bool value)
+    {
+        outline.enabled = value;
+    }
+
+    public void RecieveAttack()
+    {
+        if (deathParticles == null)
+            deathParticles.Play();
+        else
+            Destroy(this.gameObject);
+    }
+}
