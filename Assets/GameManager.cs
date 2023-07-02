ddd31c36c7a4d7e625ac592302d2d80c028973f8
ianco99 <71154684+ianco99@users.noreply.github.com>
2023-07-02 16:55:09 -0300

    Fixed GameManager todo's

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,39 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviourSingleton<GameManager>
 {
-    [SerializeField]
-    private int level1BuildIndex = 1;
+    [SerializeField] private int MenuBuildIndex = 0;
 
-    //TODO: Fix - Receive index as parameter
-    public void LoadLevel()
+    public void LoadLevel(int sceneIndex)
     {
-        SceneManager.LoadScene(1);
-        //TODO: TP2 - Remove unused methods/variables/classes
-        //UnloadLevel();
-    }
-
-    private void UnloadLevel()
-    {
-        //TODO: Fix - Hardcoded value
-        SceneManager.UnloadSceneAsync(0);
-        //TODO: TP2 - Remove unused methods/variables/classes
-        //SceneManager.UnloadSceneAsync(0, UnloadSceneOptions.UnloadAllEmbeddedSceneObjects);
+        SceneManager.LoadScene(sceneIndex);
     }
 
     public void LoadMenu()
     {
         //TODO: Fix - Hardcoded value
-        SceneManager.LoadScene(0);
-    }
-
-    public void LoadGameLevel(bool LoadNext)
-    {
-        //TODO: TP2 - Remove unused methods/variables/classes
-        Scene previousScene = SceneManager.GetActiveScene();
-        //TODO: Fix - Trash code
-        if (LoadNext)
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        else
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        SceneManager.LoadScene(MenuBuildIndex);
     }
 }
