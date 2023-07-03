a69c497c654e30f6dd1cb5f04d1412e37ca1a0e6
ianco99 <71154684+ianco99@users.noreply.github.com>
2023-07-02 21:50:54 -0300

    Created camera settings SO

---
+++ Assets/Data/CameraSettings.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CameraSettingsData", menuName = "ScriptableObjects/CameraSettings")]
+public class CameraSettings : ScriptableObject
+{
+    [Header("Controller")]
+    public float controllerYAxisMaxSpeed = 0.1f;
+    public float controllerXAxisMaxSpeed = 3.0f;
+
+    [Header("Mouse and Keyboard")]
+    public float mouseYAxisMaxSpeed = 0.01f;
+    public float mouseXAxisMaxSpeed = 0.2f;
+}
